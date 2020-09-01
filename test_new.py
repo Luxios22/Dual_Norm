6297527b8a041c22e9977060053a10eff2a6e47b
Lin Shan <LinShanify@gmail.com>
2020-09-01 11:28:45 +0800

    Fix Device Issue

--- test_new.py
+++ test_new.py
@@ -250,16 +250,11 @@ def test(config_file = 'config/dual_norm.yaml', num_classes = 18530, number_fold
                 with torch.no_grad():
                     images, pids, camids = data
                     n, c, h, w = images.size()
-                    if device:
-                        model.to(device)
-                        images = images.to(device)
-
-
-
                     ff = torch.FloatTensor(n,2048).zero_()
                     for i in range(2):
                         if(i==1):
                             images = fliplr(images)
+                        model.to(device)
                         input_img = images.to(device)
                         outputs = model(input_img)
                         f = outputs.data.cpu()
