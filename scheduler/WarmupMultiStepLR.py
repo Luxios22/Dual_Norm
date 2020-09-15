0b4028aa005605371f8f58473ee920aee43a7395
Lin Shan <LinShanify@gmail.com>
2020-09-15 13:47:02 +0800

    update

--- scheduler/WarmupMultiStepLR.py
+++ scheduler/WarmupMultiStepLR.py
@@ -1,19 +1,12 @@
-# encoding: utf-8
-"""
-@author:  liaoxingyu
-@contact: sherlockliao01@gmail.com
-"""
 from bisect import bisect_right
 import torch
-
-from torch.optim.lr_scheduler import _LRScheduler
 
 
 # FIXME ideally this would be achieved with a CombinedLRScheduler,
 # separating MultiStepLR with WarmupLR
 # but the current LRScheduler design doesn't allow it
 
-class WarmupMultiStepLR(_LRScheduler):
+class WarmupMultiStepLR(torch.optim.lr_scheduler._LRScheduler):
     def __init__(
         self,
         optimizer,
@@ -43,41 +36,16 @@ class WarmupMultiStepLR(_LRScheduler):
         super(WarmupMultiStepLR, self).__init__(optimizer, last_epoch)
 
     def get_lr(self):
-        warmup_factor = _get_warmup_factor_at_iter(
-            self.warmup_method, self.last_epoch, self.warmup_iters, self.warmup_factor
-        )
+        warmup_factor = 1
+        if self.last_epoch < self.warmup_iters:
+            if self.warmup_method == "constant":
+                warmup_factor = self.warmup_factor
+            elif self.warmup_method == "linear":
+                alpha = self.last_epoch / self.warmup_iters
+                warmup_factor = self.warmup_factor * (1 - alpha) + alpha
         return [
-            base_lr * warmup_factor * self.gamma ** bisect_right(self.milestones, self.last_epoch)
+            base_lr
+            * warmup_factor
+            * self.gamma ** bisect_right(self.milestones, self.last_epoch)
             for base_lr in self.base_lrs
         ]
-
-    
-    def _compute_values(self):
-        # The new interface
-        return self.get_lr()
-
-def _get_warmup_factor_at_iter(
-        method: str, iter: int, warmup_iters: int, warmup_factor: float
-) -> float:
-    """
-    Return the learning rate warmup factor at a specific iteration.
-    See https://arxiv.org/abs/1706.02677 for more details.
-    Args:
-        method (str): warmup method; either "constant" or "linear".
-        iter (int): iteration at which to calculate the warmup factor.
-        warmup_iters (int): the number of warmup iterations.
-        warmup_factor (float): the base warmup factor (the meaning changes according
-            to the method used).
-    Returns:
-        float: the effective warmup factor at the given iteration.
-    """
-    if iter >= warmup_iters:
-        return 1.0
-
-    if method == "constant":
-        return warmup_factor
-    elif method == "linear":
-        alpha = iter / warmup_iters
-        return warmup_factor * (1 - alpha) + alpha
-    else:
-        raise ValueError("Unknown warmup method: {}".format(method))
